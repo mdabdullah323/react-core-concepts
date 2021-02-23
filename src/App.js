a3d748507b0bca28f979d1a6ce87bccddeab8c65
Md.Abdullah <abdullah15-3842@diu.edu.bd>
2021-02-24 02:21:17 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,69 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const nayok= ['Razzak','Josim','Nadia','Abdullah','omar','ibrahim']
+  const Products = [
+    {name : 'photoShop',price :'$90.99'},
+    {name : 'Ilastrator',price :'$60.99'},
+    {name : 'pdfReader',price :'$30.99'},
+]
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p> I am a React-Person </p>
+        <Counter></Counter>
+        <Users></Users>
+        <ul>
+          {
+            nayok.map(nayok =><li>{nayok}</li>)
+          }
+          
+        </ul>
+        <Product Product={Products[0]}> </Product>
+        <Product Product={Products[1]}> </Product>
+        <Product Product={Products[2]}> </Product>
+
       </header>
     </div>
   );
 }
+function Counter(){
+  const [count,setCount] = useState(10);
+  return (
+    <div>
+      <h1> Count : {count}</h1>
+    </div>
+  )
+}
+function Users(){
+  const [user,setUsers] =useState([]);
+  return (
+    <div>
+      <h3> Dynamic users </h3>
+    </div>
+  )
+}
+function Product (props){
+  const productStyle= {
+    border:'1px solid gray',
+    backgroundColor: 'lightgray',
+    borderRadius:'5px',
+    height:'200px',
+    width:'200px',
+    float:'left'
+  }
+  return (
+    <div style ={productStyle}>
+      <h3>  {props.Product.name}</h3>
+      <h5>{props.Product.price}</h5>
+      <button> Buy now </button>
+
+    </div>
+  )
+}
 
 export default App;
+
